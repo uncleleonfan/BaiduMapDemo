93bb07b7e5c8619a04721dadb775a6a8b2ee7723
Leon <526247082@qq.com>
2016-11-21 17:30:07 +0800

    Init SDK。

---
+++ app/src/main/java/com/itheima/bdmdemo/BDMApplication.java
@@ -0,0 +1,20 @@
+package com.itheima.bdmdemo;
+
+import android.app.Application;
+
+import com.baidu.mapapi.SDKInitializer;
+
+/**
+ * 创建者:   Leon
+ * 创建时间:  2016/11/21 17:16
+ * 描述：    TODO
+ */
+public class BDMApplication extends Application {
+    public static final String TAG = "BDMApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SDKInitializer.initialize(getApplicationContext());
+    }
+}
